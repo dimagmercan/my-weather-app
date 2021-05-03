4192719b3945d3b3724c0c4fddabca2b42dda0e9
dimagmercan <dimagm@gmail.com>
2021-05-03 08:52:39 +0200

    Created html&css&js

---
+++ src/script.js
@@ -0,0 +1,97 @@
+let now = new Date();
+let date = now.getDate();
+let year = now.getFullYear();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tueday",
+  "Wednesday",
+  "Thuesday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+let minute = now.getMinutes();
+let hour = now.getHours();
+
+let pDay = document.querySelector("p.day");
+pDay.innerHTML = `${date} ${month} ${year} ${day}`;
+let pTime = document.querySelector("p.time");
+pTime.innerHTML = `${hour}:${minute}`;
+
+function convertFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = temperatureElement.innerHTML;
+  temperature = Number(temperature);
+  temperatureElement.innerHTML = Math.round((temperature * 9) / 5 + 32);
+}
+
+let fahrenheit = document.querySelector("#fahrenheit");
+fahrenheit.addEventListener("click", convertFahrenheit);
+
+function displayWeather(response) {
+  console.log(response.data);
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#feels-like").innerHTML = Math.round(
+    response.data.main.feels_like
+  );
+  document.querySelector("#temp-max").innerHTML = Math.round(
+    response.data.main.temp_max
+  );
+  document.querySelector("#temp-min").innerHTML = Math.round(
+    response.data.main.temp_min
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+function search(event) {
+  event.preventDefault();
+  let apiKey = "68a71e49014eeae4c893e057a0bfa06d";
+  let cityInput = document.querySelector("#search-text-input");
+  let city = cityInput.value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
+
+let searchButton = document.querySelector("#search-button");
+searchButton.addEventListener("click", search);
+
+function searchLocation(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiKey = "68a71e49014eeae4c893e057a0bfa06d";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function getLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let locationButton = document.querySelector("#location-button");
+locationButton.addEventListener("click", getLocation);
