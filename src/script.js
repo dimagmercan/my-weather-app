f0847be77f2e4525090f856ed17279a91ece99ec
dimagmercan <dimagm@gmail.com>
2021-05-14 15:25:10 +0200

    JS forecast container margin fix

--- src/script.js
+++ src/script.js
@@ -60,7 +60,7 @@ function formatDay(timestamp) {
 function displayForecast(response) {
   let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
-  let forecastHTML = `<div class="row mb-3">`;
+  let forecastHTML = `<div class="row">`;
 
   forecast.forEach(function (forecastDay, index) {
     if (index < 6 && index > 0) {
